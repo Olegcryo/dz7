aa04cb610e7ade04e4adea9fcd5a168c54e11e9b
Oleg <zerafperon@gmail.com>
2023-02-11 12:11:19 +0400

    add num1

---
+++ num1/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+// m = 3, n = 4.
+
+// 0,5 7 -2 -0,2
+
+// 1 -3,3 8 -9,9
+
+// 8 7,8 -7,1 9
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+
+            System.Console.Write($"{array[i, j]}\t");
+
+        }
+        System.Console.WriteLine();
+    }
+}
+int[,] CreateMatrix()
+{
+    int[,] matrix = new int[3, 4];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(1, 10);
+        }
+    }
+    return matrix;
+}
+
+int[,] matrix = CreateMatrix();
+PrintArray(matrix);
